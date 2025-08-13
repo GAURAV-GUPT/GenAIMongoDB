c18c0ccfd6323af422b1c53877bd65f59a50e3a6
GAURAV-GUPT <162366227+GAURAV-GUPT@users.noreply.github.com>
2025-08-13 20:57:23 +0100

    Update key_param.py

--- key_param.py
+++ key_param.py
@@ -1,2 +1,1 @@
-openai_api_key = "ENTER_OPENAI_API_KEY_HERE"
 MONGO_URI = "mongodb+srv://ggauravonline:ZjzMYcey6uIRhhTZ@gagcluster.xitqzxh.mongodb.net/?retryWrites=true&w=majority&appName=GAGCluster"
